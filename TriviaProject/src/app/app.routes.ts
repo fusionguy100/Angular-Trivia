b536c018776b712465852410776f2681bbaf127d
Jacob Newell <fusionguy200@hotmail.com>
2025-10-14 08:04:09 -0700

    Add routes

--- TriviaProject/src/app/app.routes.ts
+++ TriviaProject/src/app/app.routes.ts
@@ -1,3 +1,22 @@
-import { Routes } from '@angular/router';
+import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './Components/home-component/home-component';
+import { LeaderboardComponent } from './Components/leaderboard-component/leaderboard-component';
+import { QuizComponent } from './Components/quiz-component/quiz-component';
+import { ResultsComponent } from './Components/results-component/results-component';
+import { SettingsComponent } from './Components/settings-component/settings-component';
+import { NgModule } from '@angular/core';
+import { Router } from 'express';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {path: "", component: HomeComponent},
+  {path: "settings", component: SettingsComponent},
+  {path: "leaderboard", component: LeaderboardComponent},
+  {path: "results", component: ResultsComponent},
+  {path: "quiz", component: QuizComponent},
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule {}
